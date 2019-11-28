c3dcd93247a1d2c2a93e9bf774ee9dab34f0b990
Haley Serr <haleykpelletier@gmail.com>
2019-11-28 14:48:52 -0600

    concert-this and spotify-this-song oerational, some bugs

--- liri.js
+++ liri.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 const axios = require("axios");
 const fs = require("fs");
+var Spotify = require('node-spotify-api');
 
 //Required Things^
 
-// Variables
+// Command Line Variables
 
 const action = process.argv[2];
 const value = process.argv[3];
+
+
+//Super Switch
 
 switch (action) {
     case "concert-this":
@@ -17,53 +21,67 @@ switch (action) {
         spotifyThisSong();
 
     case "movie-this":
-        movieThis();
+       movieThis();
 
     case "do-what-it-says":
-        doWhatItSays();
+       doWhatItSays();
 
-}
+};
 
 // `concert-this`
 //`node liri.js concert-this <artist/band name here>`
-
-// This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-
-// Name of the venue
-
-// Venue location
-
 // Date of the Event (use moment to format this as "MM/DD/YYYY")
 
 function concertThis() {
 
+    axios.get(`https://rest.bandsintown.com/artists/${value}/events?app_id=${process.env.BANDS_IN_TOWN_APP_ID}`).then(
+       
+    function (response) {
 
-}
+            console.log("The next conert is at " + response.data[0].venue.name + " in " + response.data[0].venue.country + " on " + response.data[0].datetime)
 
+        });
 
+};
 
 
 // `spotify-this-song`
 //`node liri.js spotify-this-song '<song name here>'`
-
-//var spotify = new Spotify(keys.spotify);
-
-// This will show the following information about the song in your terminal/bash window
-
-// Artist(s)
-
-// The song's name
-
-// A preview link of the song from Spotify
-
-// The album that the song is from
-
 // If no song is provided then your program will default to "The Sign" by Ace of Base.
 
 function spotifyThisSong() {
 
+    var spotify = new Spotify({
+        id: process.env.SPOTIFY_ID,
+        secret: process.env.SPOTIFY_SECRET
+      });
 
-}
+    if (process.argv.length > 3) {
+
+        spotify.search({ type: 'track', query: `${value}` }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+           
+          console.log("Song: " + data.tracks.items[0].name); 
+          console.log("Artist: " + data.tracks.items[0].artists[0].name);
+          console.log("Album: " + data.tracks.items[0].album.name);
+          console.log(data.tracks.items[0].external_urls.spotify);
+          
+
+          });
+    
+    
+    }
+    
+    else if (process.argv.length < 4) {
+
+
+    };
+
+
+
+};
 
 
 // `movie-this`
@@ -74,34 +92,35 @@ function movieThis() {
 
     if (process.argv.length > 3) {
 
-    axios.get(`http://www.omdbapi.com/?t=${value}&y=&plot=short&apikey=${process.env.OMDB_API_KEY}`).then(
-        function (response) {
-            console.log(response.data.Title + " came out on " + response.data.Released + ".");
-            console.log("The movie's rating is " + response.data.imdbRating + " on IMDB and the Rotten Tomatoes Rating is " + response.data.Ratings[2].Value + ".");
-            console.log("The movie was produced in " + response.data.Country + " in " + response.data.Language + ".");
-            console.log("Plot: " + response.data.Plot);
-            console.log("Cast: " + response.data.Actors);
+        axios.get(`http://www.omdbapi.com/?t=${value}&y=&plot=short&apikey=${process.env.OMDB_API_KEY}`).then(
+            function (response) {
 
-        })
+                console.log(response.data.Title + " came out on " + response.data.Released + ".");
+                console.log("The movie's rating is " + response.data.imdbRating + " on IMDB and the Rotten Tomatoes Rating is " + response.data.Ratings[2].Value + ".");
+                console.log("The movie was produced in " + response.data.Country + " in " + response.data.Language + ".");
+                console.log("Plot: " + response.data.Plot);
+                console.log("Cast: " + response.data.Actors);
 
-    }
+            })
+
+    } 
     
-    else if (process.argv.length < 4)  {
+    else if (process.argv.length < 4) {
 
         defaultValue = 'Mr. Nobody';
 
         axios.get(`http://www.omdbapi.com/?t=${defaultValue}&y=&plot=short&apikey=${process.env.OMDB_API_KEY}`).then(
-        function (response) {
-            console.log(response.data.Title + " came out on " + response.data.Released + ".");
-            console.log("The movie's rating is " + response.data.imdbRating + " on IMDB and the Rotten Tomatoes Rating is " + response.data.Ratings[2].Value + ".");
-            console.log("The movie was produced in " + response.data.Country + " in " + response.data.Language + ".");
-            console.log("Plot: " + response.data.Plot);
-            console.log("Cast: " + response.data.Actors);
+            function (response) {
+                console.log(response.data.Title + " came out on " + response.data.Released + ".");
+                console.log("The movie's rating is " + response.data.imdbRating + " on IMDB and the Rotten Tomatoes Rating is " + response.data.Ratings[2].Value + ".");
+                console.log("The movie was produced in " + response.data.Country + " in " + response.data.Language + ".");
+                console.log("Plot: " + response.data.Plot);
+                console.log("Cast: " + response.data.Actors);
 
-        })
+            })
 
     }
-}
+};
 
 
 //do -what - it - says `
@@ -114,4 +133,5 @@ function movieThis() {
 
 function doWhatItSays() {
 
-}
+};
+
